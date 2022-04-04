6b8a9a0affe12ece9a4de6f09516731d9a2c6707
Cole Rossman <colerossman99@gmail.com>
2022-04-04 14:49:04 -0700

    UserContext file completed

---
+++ src/contex/UserContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from 'react';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [selectedCard, setSelectedCard] = useState();
+
+  return (
+    <GameContext.Provider value={(setSelectedCard, selectedCard)}>{children}</GameContext.Provider>
+  );
+};
+
+const useGameContext = () => {
+  const context = useContext(GameContext);
+
+  if (context === undefined) {
+    throw new Error('useGameContext must be used within a GameProvider');
+  }
+  return context;
+};
+
+export { GameProvider, useGameContext };
