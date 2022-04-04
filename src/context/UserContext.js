378a42fde40332231aee2945650d0125124ce5e5
Cole Rossman <colerossman99@gmail.com>
2022-04-04 16:18:51 -0700

    functions moved to Executepassbutton and state has been utilized within useGameContext hook

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,12 +1,26 @@
 import { createContext, useContext, useState } from 'react';
-
+import initialCards from './cards-data';
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
+  const [deck, setDeck] = useState(initialCards);
+  const [playerOneHand, setPlayerOneHand] = useState([]);
   const [selectedCard, setSelectedCard] = useState();
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
 
   return (
-    <GameContext.Provider value={(setSelectedCard, selectedCard)}>{children}</GameContext.Provider>
+    <GameContext.Provider value={(
+      setSelectedCard, selectedCard,
+      deck, setDeck,
+      playerOneHand, setPlayerOneHand,
+      playerTwoHand, setPlayerTwoHand,
+      playerThreeHand, setPlayerThreeHand,
+      from, setFrom,
+      to, setTo
+    )}>{children}</GameContext.Provider>
   );
 };
 
